42fb576e0a10fd86981262b8dee6340f0943c3ec
Bruce Tian <tianrunhe@gmail.com>
2023-02-01 16:10:29 -0800

    Add ability to import CSV into reflection phase

--- packages/client/components/RetroReflectPhase/RetroReflectPhase.tsx
+++ packages/client/components/RetroReflectPhase/RetroReflectPhase.tsx
@@ -1,9 +1,14 @@
 import graphql from 'babel-plugin-relay/macro'
 import React, {useState} from 'react'
+import {Importer, ImporterField} from 'react-csv-importer'
+import 'react-csv-importer/dist/index.css'
 import {createFragmentContainer} from 'react-relay'
 import useCallbackRef from '~/hooks/useCallbackRef'
 import {RetroReflectPhase_meeting} from '~/__generated__/RetroReflectPhase_meeting.graphql'
+import useAtmosphere from '../../hooks/useAtmosphere'
 import useBreakpoint from '../../hooks/useBreakpoint'
+import useMutationProps from '../../hooks/useMutationProps'
+import CreateReflectionMutation from '../../mutations/CreateReflectionMutation'
 import {Breakpoint} from '../../types/constEnums'
 import {phaseLabelLookup} from '../../utils/meetings/lookups'
 import MeetingContent from '../MeetingContent'
@@ -27,12 +32,15 @@ const RetroReflectPhase = (props: Props) => {
   const [callbackRef, phaseRef] = useCallbackRef()
   const [activeIdx, setActiveIdx] = useState(0)
   const isDesktop = useBreakpoint(Breakpoint.SINGLE_REFLECTION_COLUMN)
-  const {localPhase, endedAt, showSidebar, settings} = meeting
+  const {localPhase, endedAt, showSidebar, settings, id: meetingId} = meeting
   const {disableAnonymity} = settings
   if (!localPhase || !localPhase.reflectPrompts) return null
   const reflectPrompts = localPhase!.reflectPrompts
   const focusedPromptId = localPhase!.focusedPromptId
   const ColumnWrapper = isDesktop ? ReflectWrapperDesktop : ReflectWrapperMobile
+  const {onCompleted, onError, submitMutation} = useMutationProps()
+  const atmosphere = useAtmosphere()
+
   return (
     <MeetingContent ref={callbackRef}>
       <MeetingHeaderAndPhase hideBottomBar={!!endedAt}>
@@ -46,6 +54,45 @@ const RetroReflectPhase = (props: Props) => {
             {`Add ${disableAnonymity ? '' : 'anonymous'} reflections for each prompt`}
           </PhaseHeaderDescription>
         </MeetingTopBar>
+
+        <Importer
+          dataHandler={async (rows: any) => {
+            // required, receives a list of parsed objects based on defined fields and user column mapping;
+            // may be called several times if file is large
+            // (if this callback returns a promise, the widget will wait for it before parsing more data)
+            console.log('received batch of rows', rows)
+
+            rows.map((row: any, idx: number) => {
+              reflectPrompts.map(({id: promptId}) => {
+                if (row[promptId].trim()) {
+                  const input = {
+                    content: `{"blocks":[{"key":"${(Math.random() + 1)
+                      .toString(36)
+                      .substring(4)}","text":"${
+                      row[promptId]
+                    }","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}`,
+                    meetingId,
+                    promptId,
+                    sortOrder: idx
+                  }
+                  submitMutation()
+                  CreateReflectionMutation(atmosphere, {input}, {onError, onCompleted})
+                }
+              })
+            })
+
+            // mock timeout to simulate processing
+            await new Promise((resolve) => setTimeout(resolve, 500))
+          }}
+          chunkSize={10000} // optional, internal parsing chunk size in bytes
+          defaultNoHeader={true} // optional, keeps "data has headers" checkbox off by default
+          restartable={false} // optional, lets user choose to upload another file when import is complete
+        >
+          {reflectPrompts.map(({id, question}) => (
+            <ImporterField key={id} name={id} label={question} />
+          ))}
+        </Importer>
+
         <PhaseWrapper>
           <StageTimerDisplay meeting={meeting} />
           <ColumnWrapper
@@ -76,6 +123,7 @@ graphql`
     reflectPrompts {
       ...PhaseItemColumn_prompt
       id
+      question
       sortOrder
     }
   }
@@ -88,6 +136,7 @@ export default createFragmentContainer(RetroReflectPhase, {
       ...StageTimerControl_meeting
       ...PhaseItemColumn_meeting
       endedAt
+      id
       localPhase {
         ...RetroReflectPhase_phase @relay(mask: false)
       }
